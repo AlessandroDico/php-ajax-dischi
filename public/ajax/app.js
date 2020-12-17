bd05f5f0ef30786ebc83bf2e9fb67edc59437090
Ale <alessandrochb@gmail.com>
2020-12-17 16:45:25 +0100

    impostato scheletro handlebars + scheletro chiamata ajax

--- public/ajax/app.js
+++ public/ajax/app.js
@@ -93,7 +93,24 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
+$(document).ready(function () {
+  console.log('ciao'); // var source = document.getElementById("entry-template").innerHTML;
+  // var template = Handlebars.compile(source);
+  // chiamata ajax
 
+  $.ajax({
+    // url: "../forAjaXIndex/index.php",
+    url: "index.php",
+    method: "GET",
+    succes: function succes(data) {
+      console.log('ok');
+      console.log(data);
+    },
+    error: function error() {
+      console.log('errore');
+    }
+  });
+});
 
 /***/ }),
 
---
+++ src/src_js/app.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    console.log('ciao');
+
+    // var source = document.getElementById("entry-template").innerHTML;
+    // var template = Handlebars.compile(source);
+
+    // chiamata ajax
+    $.ajax({
+        // url: "../forAjaXIndex/index.php",
+        url: "index.php",
+        method: "GET",
+        succes: function(data) {
+            console.log('ok');
+            console.log(data);
+        },
+        error: function() {
+            console.log('errore');
+        }
+    })
+});
