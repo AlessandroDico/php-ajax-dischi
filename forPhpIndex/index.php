1554dc504cc98b1dbe97b784941958a32a1c5f59
Ale <alessandrochb@gmail.com>
2020-12-18 16:29:23 +0100

    aggiunta select funzionante anche sul file index.php

--- forPhpIndex/index.php
+++ forPhpIndex/index.php
@@ -11,6 +11,8 @@ include '../forAllDisc.php';
         <!-- google fonts -->
         <link rel="preconnect" href="https://fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+        <!-- jQuery -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     </head>
     <body>
         <header>
@@ -23,6 +25,13 @@ include '../forAllDisc.php';
         </header>
         <main>
             <div class="container">
+                <select class="genre-select">
+                    <option value="">All Genres</option>
+                    <option value="Rock">Rock</option>
+                    <option value="Pop">Pop</option>
+                    <option value="Jazz">Jazz</option>
+                    <option value="Metal">Metal</option>
+                </select>
                 <div class="card-container">
                     <?php foreach ($dischi as $disco) { ?>
                     <div class="box-card">
@@ -39,5 +48,16 @@ include '../forAllDisc.php';
                 </div>
             </div>
         </main>
+        <script id="entry-template" type="text/x-handlebars-template">
+                <div class="box-card">
+                    <div class="card-image-container">
+                        <img src="{{poster}}" alt="{{titolo}}">
+                    </div>
+                    <p>{{titolo}}</p>
+                    <p>{{autore}}</p>
+                    <p>{{anno}}</p>
+                </div>
+        </script>
+        <script src="../public/ajax/app.js" charset="utf-8"></script>
     </body>
 </html>
